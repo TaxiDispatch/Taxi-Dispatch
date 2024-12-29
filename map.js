fa3492ef8062c1f29ea28600489881ccc8ab1f6c
TaxiDispatch <taxi.dispatch.info@gmail.com>
2024-12-29 16:31:42 +0100

    map.js

--- map.js
+++ map.js
@@ -1,7 +1,6 @@
 var map = new maplibregl.Map({
       container: 'map',
-      style: 'https://openmaptiles.github.io/osm-bright-gl-style/style-cdn.json', // stylesheet location
-      center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
-      terrain: {"source": "raster-dem-source", "exaggeration": 0.5}
+      style: 'https://openmaptiles.github.io/osm-bright-gl-style/style-cdn.json',
+      center: [8.5456, 47.3739],
+      zoom: 11
 });
