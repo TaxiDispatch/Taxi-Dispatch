96f5c9d97ced9ff979e6b7d6bf61254e1d9005bd
TaxiDispatch <taxi.dispatch.info@gmail.com>
2024-12-29 13:00:53 +0100

    map.js

--- map.js
+++ map.js
@@ -1,7 +1,7 @@
 var map = new maplibregl.Map({
       container: 'map',
-      style: 'https://demotiles.maplibre.org/style.json', // stylesheet location
+      style: 'https://github.com/maplibre/demotiles/blob/gh-pages/styles/osm-bright-gl-style/style.json', // stylesheet location
       center: [-74.5, 40], // starting position [lng, lat]
       zoom: 9, // starting zoom
       terrain: {"source": "raster-dem-source", "exaggeration": 0.5}
-    });
+});
